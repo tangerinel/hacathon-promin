0a795dec067875100b9b6d65f46aa8d0e12ec6b0
ohaponiuk <sarasei@pm.me>
2023-03-10 23:52:12 +0000

    Setup hardhat config

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { resolve } from "path";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
