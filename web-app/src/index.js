64547c5b09adaa71f2d585f67c3b985adcdcf772
tangerinel <lizaskorohod5@gmail.com>
2023-03-11 18:35:07 +0000

    fixed boundler

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,51 +1,13 @@
 "use strict";
 const express = require('express');
 const Bundler = require('parcel-bundler');
-
-
-
-
-
-// declare module "express-session" {
-//   export interface SessionData {
-//     userId: ObjectId;
-//   }
-// }
-
-// /** Connect to Mongo */
-// mongoose
-//   .connect(config.mongo.url)
-//   .then(() => {
-//     console.log("Mongo connected successfully.");
-//     startServer();
-//   })
-//   .catch((error) => console.log(error));
+const { join } = require( "path");
 
 /** starts server if mongoose connected */
 const startServer = () => {
   const app = express();
 
-  //to use cors instead of express.static uncoment this
-  // app.use(
-  //   cors({
-  //     origin: ["http://localhost:8080"],
-  //     credentials: true,
-  //   })
-  // );
-
-//   app.use(
-//     session({
-//       store: MongoStore.create({ mongoUrl: config.mongo.url }),
-//       secret: "very secret secret",
-//       resave: false,
-//       saveUninitialized: false,
-//       cookie: {
-//         maxAge: 1000 * 60 * 60 * 3, // 3 hours
-//         httpOnly: true,
-//       },
-//     })
-//   );
-  const file = '../static/login.html';
+  const file = join(__dirname, "../static/login.html");
   const options = {};
 
   const bundler = new Bundler(file, options);
@@ -54,5 +16,6 @@ const startServer = () => {
     console.log(`SERVER STARTED ON PORT ${3000}`)
   );
 };
+
 startServer();
  
